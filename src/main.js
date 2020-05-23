ad39bfb6ba72ae9d687cd6c263e2b4f31f2f53c7
Andr3sdosSantos <andres_santosbritoamaral@hotmail.com>
2020-05-23 17:28:27 -0300

    :taba: All features Electron

---
+++ src/main.js
@@ -0,0 +1,42 @@
+const { app, BrowserWindow, globalShortcut } = require('electron')
+const host = require('./config')
+
+let window;
+
+function createWindow () {
+  window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    titleBarStyle: 'hidden',
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  window.loadURL(host.url)
+}
+
+function toggleDevTools() {
+  window.webContents.toggleDevTools()
+}
+
+function createShortcuts() {
+  globalShortcut.register('CmdOrCtrl+J', toggleDevTools)
+}
+
+app.whenReady()
+  .then(createWindow)
+  .then(createShortcuts)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
